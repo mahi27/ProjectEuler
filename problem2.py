1f2168af1f6ac4d0699377eb80b4972010cf3d61
Mahitha <mahitasree@gmail.com>
2018-02-11 19:48:41 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -5,21 +5,22 @@ Created on Sun Apr 23 20:45:52 2017
 @author: Mahhita
 """
 #%%
-a = []
-a.append(1)
-a.append(2)
-#%%
-i = 2
-while i > 0:
-    s = a[i - 1] + a[i - 2]
-    if s < 4000000:
-        a.append(s)
-        i = i+1
-    else:
-        break
-#%%
-sum = 0
-for i in a:
-    if i%2 == 0:
-        sum = sum + i
-#%%
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = []
+    a.append(1)
+    a.append(2)
+    i = 2
+    while i > 0:
+        s = a[i - 1] + a[i - 2]
+        if s < n:
+            a.append(s)
+            i = i+1
+        else:
+            break
+    s = 0
+    for i in a:
+        if i%2 == 0:
+            s = s + i
+    print(s)
