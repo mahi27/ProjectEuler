0be89359750fa6e15617d42b3e725fbf1838278b
Mahitha <mahitasree@gmail.com>
2018-02-11 19:43:26 +0530

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -7,9 +7,16 @@ Created on Sun Apr 23 20:28:31 2017
 
 #Problem1
 #%%
-sum = 0
-for i in range(1,1000):
-    if (i%3 == 0) or (i%5 == 0):
-        sum = sum + i
+def art(y):
+    return y*(y+1);
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    n -=1;
+    a=int(n/3);
+    b=int(n/5);
+    c=int(n/15);
+    print(int(int(3*art(a) + 5*art(b) - 15*art(c))>>1));
         
 #%%
