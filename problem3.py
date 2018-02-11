dc60b9e12ee12e7270ae19ba9484fa2d88fd7202
Mahitha <mahitasree@gmail.com>
2018-02-11 20:21:19 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -10,13 +10,17 @@ Created on Sun Apr 23 21:07:43 2017
 Problem 3
 """
 
-n = 600851475143
-i = 2
-while i * i < n:
-     while n % i == 0:
-         n = n / i
-     i = i + 1
-print (n)
+for t in range(int(input())):
+    n = int(input())
+    i = 2
+    high = 1
+    while(i*i <= n):
+        while(n%i == 0 ):
+            n = n/i
+            high = i
+        i += 1
+    if n > 1 : print(int(n))
+    else: print(high)
 #%%
 
 
@@ -25,10 +29,18 @@ Problem 4
 """
 def palindrome(r):
     return(list(str(r)) == list(reversed(str(r))))
-#%%
-product = 10000
-for i in range(100,1000):
-    for j in range(100,1000):
-        pr = i*j
-        if palindrome(pr) and pr > product:
-            product = pr
+
+list_palindrome = []
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        temp = i * j
+        if palindrome(temp):
+            list_palindrome.append(temp)
+list_palindrome.sort()
+
+for _ in range(int(input())):
+    n = int(input())
+    for i in range(len(list_palindrome)):
+        if list_palindrome[i] >= n:
+            print(list_palindrome[i-1])
+            break
